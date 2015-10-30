65f11839c3b9377ad4e6f809b14b11b4450acee4
Hoang Ngoc Tung <tungmdc55@gmail.com>
2015-10-30 12:46:19 +0700

    First Commit

--- RunningLed/ViewController.m
+++ RunningLed/ViewController.m
@@ -9,19 +9,71 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+{
+    CGFloat _magrin;
+    int _numberOfBall;
+    CGFloat _diameter;
+    CGFloat _space;
+    NSTimer * _timer;
+    int lastOnLed;
+}
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    _magrin =40;
+    _diameter =24;
+    _timer = [NSTimer scheduledTimerWithTimeInterval:0.5 target:self selector:@selector(runningLed) userInfo:nil repeats:true];
+    _numberOfBall =8;
+    lastOnLed =0;
+    [self drawOfBall:_numberOfBall];
+}
+-(void)runningLed {
+    if (lastOnLed != _numberOfBall) {
+        [self turnOffLed:lastOnLed];
+    }
+    
+    if (lastOnLed != 0) {
+        lastOnLed--;
+    } else {
+        lastOnLed = _numberOfBall-1;
+    }
+    [self turnOnLed:lastOnLed];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void) turnOnLed: (int) index {
+    UIView* view = [self.view viewWithTag:index +100];
+    if (view && [view isMemberOfClass:[UIImageView class]]) {
+        UIImageView* ball = (UIImageView*) view;
+        ball.image = [UIImage imageNamed:@"green"];
+    }
 }
-
+-(void) turnOffLed: (int) index {
+    UIView* view = [self.view viewWithTag:index +100];
+    if (view && [view isMemberOfClass:[UIImageView class]]) {
+        UIImageView* ball = (UIImageView*) view;
+        ball.image = [UIImage imageNamed:@"gray"];
+    }
+}
+- (void)placeBallAtX: (CGFloat) x
+                andY: (CGFloat) y
+             withTag: (int) tag {
+    UIImageView* ball = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"gray"]];
+    ball.center = CGPointMake(x, y);
+    ball.tag = tag;
+    [self.view addSubview:ball];
+}
+-(CGFloat) spaceBetweenBall: (int) n {
+    return (self.view.bounds.size.width - 2 * _magrin) / (n-1);
+}
+-(void) drawOfBall: (int) numberBalls {
+    CGFloat space = [self spaceBetweenBall:_numberOfBall];
+    for (int i=0; i< numberBalls; i++) {
+        [self placeBallAtX: _magrin + i*space
+                      andY:150
+                   withTag:i+100];
+    }
+}
 @end
